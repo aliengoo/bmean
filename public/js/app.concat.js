f8c66f6ff758ea419c79f3be811b539f3b22fb1f
Glenn Bunter <glenn.android@gmail.com>
2014-04-22 21:58:22 +0100

    Removed grunted files from git.

--- public/js/app.concat.js
+++ public/js/app.concat.js
@@ -1,7 +1,7 @@
 (function () {
   "use strict";
 
-  window.app = angular.module('app', ['ngResource', 'ngCookie', 'ngAnimate', 'ui.router', 'ui.utils', 'ui.bootstrap'])
+  window.app = angular.module('app', ['ngResource', 'ngCookies', 'ngAnimate', 'ui.router', 'ui.utils', 'ui.bootstrap'])
     .config(['$provide', '$stateProvider', '$urlRouterProvider', function ($provide, $stateProvider, $urlRouterProvider) {
 
       // TODO : Handle uncaught exceptions here
@@ -51,7 +51,47 @@
 ;(function () {
   "use strict";
 
-  window.app.controller('homeCtrl', ['$scope', function ($scope) {
+  window.app.factory('toastrSvc', [function () {
+    toastr.options = {
+      "closeButton": true,
+      "debug": false,
+      "positionClass": "toast-bottom-right",
+      "onclick": null,
+      "showDuration": "300",
+      "hideDuration": "1000",
+      "timeOut": "8000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    };
+
+    return {
+      info: function (message, title) {
+        toastr.warning(message, title);
+      },
+      success: function (message, title) {
+        toastr.success(message, title);
+      },
+      warning: function (message, title) {
+        toastr.warning(message, title);
+      },
+      error: function (message, title) {
+        toastr.error(message, title);
+      },
+      clear: function () {
+        toastr.clear();
+      }
+    };
+  }]);
+}());
+;(function () {
+  "use strict";
+
+  window.app.controller('homeCtrl', ['$scope', 'toastrSvc', function ($scope, toastrSvc) {
     $scope.message = 'BMEAN';
+
+    toastrSvc.success('Hello', 'World!');
   }]);
 }());
