c086eb74c31bed12d1de00a91aa3566bcbb12342
Glenn bunter <gbunter@teachersassurance.co.uk>
2014-04-24 07:45:58 +0100

    Updated vagrant bootstrap.sh to handle re-provisioning

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-sudo apt-get -y install python-software-properties python g++ make
+# prevent provisioning running again
+if [ -f "/var/vagrant_provision" ]; then
+  exit 0
+fi
+
+sudo apt-get -y install software-properties-common python-software-properties python g++ make
 sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
@@ -9,3 +14,5 @@ sudo apt-get -y install mongodb-org
 sudo apt-get -y install nodejs
 sudo npm install nodemon -g
 sudo npm install forever -g
+
+touch /var/vagrant_provision
