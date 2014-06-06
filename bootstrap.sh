c1b1385cc18705bf667e28960f2fea26ffbcb9cd
aliengoo <glenn.android@gmail.com>
2014-06-06 12:32:52 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# prevent provisioning running again
+# check if provisioning has already been completed
 if [ -f "/var/vagrant_provision" ]; then
   exit 0
 fi
@@ -12,7 +12,10 @@ echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | 
 sudo apt-get -y update
 sudo apt-get -y install mongodb-org
 sudo apt-get -y install nodejs
+sudo apt-get -y install npm
+sudo npm config set registry http://registry.npmjs.org/
 sudo npm install nodemon -g
 sudo npm install forever -g
 
+# flag that provisioning has been completed
 touch /var/vagrant_provision
